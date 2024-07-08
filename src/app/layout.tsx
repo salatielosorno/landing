b522c543e75c8b800e02c3bc3958ec46d049cbd6
salatielosorno <salatiel.linares@gmail.com>
2024-07-07 23:27:17 -0600

    feat(fonts): add font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Quicksand } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const quicksand = Quicksand({
+  subsets: ['latin'],
+  variable: '--font-quicksand',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#F2E8DF]`}>{children}</body>
+      <body className={`${quicksand.variable} bg-[#F2E8DF]`}>{children}</body>
     </html>
   );
 }
